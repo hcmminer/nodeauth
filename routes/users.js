ab35550baa9979f1a2579f35e22964c0ba7fa5d5
Your Name <you@example.com>
2021-03-10 14:42:05 +0700

    lecture 15

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource ok');
+});
+router.get('/register', function(req, res, next) {
+  res.render('register',{title: "register"});
+});
+router.get('/login', function(req, res, next) {
+  res.render('login',{title: "login"});
+});
+router.get('/logout', function(req, res, next) {
+  res.render('logout',{title: "logout"});
 });
 
 module.exports = router;
