e75193e71557efb9623ad6cfcc76f44062c8249e
Your Name <you@example.com>
2021-03-15 22:37:28 +0700

    done fix length of undifined

--- routes/users.js
+++ routes/users.js
@@ -43,7 +43,6 @@ router.post(
 		// Finds the validation errors in this request and wraps them in an object with handy functions
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
-			// return res.status(400).json({ errors: errors.array() });
 			res.render("register",{a: "AAAA",b:"BBB",errors: errors.array()});
 		}
 		// save data to databases
