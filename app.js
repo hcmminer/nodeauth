8202c938d42d2a3b536dae689e4374fa9e250e9e
Your Name <you@example.com>
2021-03-31 02:38:45 +0700

    end lecture 19

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
+// packages
+const express = require("express");
+const passport = require("passport");
+const path = require("path");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const passport = require('passport');
 
-//EXPRESS
-const express = require("express");
 const app = express();
 const port = 3000;
 
-// VIEW ENGINE SETUP
-const path = require("path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-// SESSION-EXPRESS
-const session = require("express-session");
-app.set("trust proxy", 1); // trust first proxy
+app.set("trust proxy", 1);
 app.use(
 	session({
 		secret: "keyboard cat",
@@ -24,18 +24,14 @@ app.use(
 	})
 );
 
-// COOKIE PARSE AFTER SESSION
-const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-// BODY PARSE
-const bodyParser = require("body-parser");
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-//
+// passport init
 app.use(passport.initialize());
 
 // ROUTER
